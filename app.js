db43efd4264ef98c940af42f9dfbea45155295f9
annususman13 <annususman42@gmail.com>
2020-06-29 13:15:37 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function calc(num){
+    var cal = document.getElementById("input");
+    
+
+    input.value += num;
+}
+
+function clr(){
+    var clr = document.getElementById("input");
+    input.value = ""
+}
+function result(){
+    {input.value = (eval(input.value))}
+}
